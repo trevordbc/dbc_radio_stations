391edf6c0d0837c736d6aa58806f47c0ae286a52
trevordbc <trevor@dbcnext.com>
2023-04-25 09:04:34 -0400

    Update audio_player.js

--- javascript/audio_player.js
+++ javascript/audio_player.js
@@ -1,113 +1,131 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+	if (typeof elementorPodcastData !== "undefined") {
     const player = document.getElementById('player');
-    const currentTimeElement = $('#current-time');
-    const durationElement = $('#duration');
-    const podcastTitleElement = $('#podcast-title');
     const seeker = document.getElementById('seekbar');
-
-    function playPodcast(src, title) {
-        player.src = src;
-        player.play();
-        podcastTitleElement.text(title);
-    }
+    const currentTimeElement = document.getElementById('current-time');
+    const durationElement = document.getElementById('duration');
+    const podcastTitleElement = document.getElementById('podcast-title');
+    const playingIndicator = document.getElementById('playing-indicator');
+    const pagination = document.getElementById('pagination');
+    let currentPlayingItem = null;
 
     function resetPlayer() {
         player.pause();
-        player.currentTime = 0;
+        player.src = '';
         seeker.value = 0;
+        currentTimeElement.textContent = '00:00';
+        durationElement.textContent = '00:00';
     }
 
-    elementorPodcastData.podcasts.forEach((podcast, index) => {
-        const accordionTitle = $('.elementor-accordion-title[data-rss-feed="' + podcast.url + '"]');
-        const sectionToggle = accordionTitle.data('section-toggle');
+    function playPodcast(enclosure, title) {
+        resetPlayer();
+        player.src = enclosure;
+        podcastTitleElement.textContent = title;
+        player.play();
+    }
 
-        if (userHasAccess(podcast.allowed_roles)) {
-            let playlistHtml = '';
-
-            podcast.items.forEach((item, itemIndex) => {
-                const duration = formatDuration(item.duration);
-                playlistHtml += `
-                    <div class="playlist-item" data-src="${item.enclosure}">
-                        <div class="item-title">${item.title}</div>
-                        <div class="item-duration">${duration}</div>
-                    </div>
-                `;
-            });
-
-            $('#' + sectionToggle + ' .custom-accordion-container').html(playlistHtml);
-
-            $('#' + sectionToggle + ' .playlist-item').on('click', function () {
-                const src = $(this).data('src');
-                const title = $(this).find('.item-title').text();
-                playPodcast(src, title);
-
-                $('.playlist-item').removeClass('playing');
-                $(this).addClass('playing');
-            });
-        }
-    });
-
-    player.addEventListener('timeupdate', function () {
-        seeker.value = (player.currentTime / player.duration) * 100;
-        currentTimeElement.text(formatDuration(player.currentTime));
-    });
-
-    seeker.addEventListener('input', function () {
-        player.currentTime = (seeker.value / 100) * player.duration;
-    });
-
-    player.addEventListener('loadedmetadata', function () {
-        durationElement.text(formatDuration(player.duration));
-    });
-
-    player.addEventListener('ended', function() {
-        let currentPodcast = $('.playlist-item.playing');
-        let nextPodcast = currentPodcast.next('.playlist-item');
-
-        if (nextPodcast.length === 0) {
-            resetPlayer();
-        } else {
-            const src = nextPodcast.data('src');
-            const title = nextPodcast.find('.item-title').text();
-            playPodcast(src, title);
-            currentPodcast.removeClass('playing');
-            nextPodcast.addClass('playing');
-        }
-    });
-
-    function formatDuration(seconds) {
+    function formatTime(seconds) {
         const minutes = Math.floor(seconds / 60);
         seconds = Math.floor(seconds % 60);
-
         return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
 
-    function userHasAccess(allowedRoles) {
-        if (!Array.isArray(allowedRoles) || allowedRoles.length === 0) {
-            return true;
+    function displayPlaylistItems(podcast) {
+        if (!podcast || !podcast.items || podcast.items.length === 0) {
+            return '';
         }
 
-        return elementorPodcastData.userRoles.some(role => allowedRoles.includes(role));
+        return podcast.items.map((item, index) => {
+            const formattedDuration = formatTime(item.duration);
+            return `<div class="playlist-item" data-src="${item.enclosure}" data-index="${index}">
+                <div class="podcast-title">${item.title}</div>
+                <div class="podcast-duration">${formattedDuration}</div>
+            </div>`;
+        }).join('');
     }
-	const firstAccordion = $('.elementor-accordion-title').first();
-const firstAccordionSection = firstAccordion.data('section-toggle');
-const firstAccordionPodcast = elementorPodcastData.podcasts.find(p => p.url === firstAccordion.data('rss-feed'));
 
-if (firstAccordionPodcast && firstAccordionPodcast.items.length > 0) {
-    const firstPodcast = firstAccordionPodcast.items[0];
-    playPodcast(firstPodcast.enclosure, firstPodcast.title);
+    elementorPodcastData.podcasts.forEach((podcast, index) => {
+        const accordionContent = $(`.elementor-accordion .elementor-accordion-item:eq(${index}) .custom-accordion-container`);
+        accordionContent.html(displayPlaylistItems(podcast));
+    });
 
-    $('#' + firstAccordionSection + ' .playlist-item').first().addClass('playing');
-}
+    // Play, Pause, and Stop controls
+    $('#play').on('click', function () {
+        player.play();
+    });
 
-$('.elementor-accordion-title').on('click', function() {
-    const sectionToggle = $(this).data('section-toggle');
-    const isOpen = $('#' + sectionToggle).hasClass('elementor-active');
+    $('#pause').on('click', function () {
+        player.pause();
+    });
 
-    if (isOpen) {
-        $('#' + sectionToggle + ' .playlist-item').first().click();
-    } else {
+    $('#stop').on('click', function () {
         resetPlayer();
+    });
+
+    // Seeker and time updates
+    player.addEventListener('timeupdate', function () {
+        seeker.value = player.currentTime / player.duration * 100;
+        currentTimeElement.textContent = formatTime(player.currentTime);
+    });
+
+    seeker.addEventListener('input', function () {
+        player.currentTime = player.duration * (seeker.value / 100);
+    });
+
+    player.addEventListener('loadedmetadata', function () {
+        durationElement.textContent = formatTime(player.duration);
+    });
+
+    // Playlist items click
+    $('.elementor-accordion').on('click', '.playlist-item', function () {
+        if (currentPlayingItem) {
+            currentPlayingItem.removeClass('playing');
+        }
+        currentPlayingItem = $(this);
+        currentPlayingItem.addClass('playing');
+
+        const src = $(this).data('src');
+        const title = $(this).find('.podcast-title').text();
+        playPodcast(src, title);
+    });
+
+    // Initialize the first podcast
+    const firstAccordion = $('.elementor-accordion-title').first();
+    const firstAccordionSection = firstAccordion.data('section-toggle');
+    const firstAccordionPodcast = elementorPodcastData.podcasts.find(p => p.url === firstAccordion.data('rss-feed'));
+	
+	if (firstAccordionPodcast && firstAccordionPodcast.items.length > 0) {
+        const firstPodcast = firstAccordionPodcast.items[0];
+        podcastTitleElement.textContent = firstPodcast.title;
+        player.src = firstPodcast.enclosure;
+        currentPlayingItem = $(`.elementor-accordion .elementor-accordion-item:eq(0) .playlist-item[data-index="0"]`);
+        currentPlayingItem.addClass('playing');
     }
-});
+
+    // Automatically move to the next podcast when one ends
+    player.addEventListener('ended', function () {
+        if (!currentPlayingItem) {
+            return;
+        }
+
+        const nextIndex = currentPlayingItem.data('index') + 1;
+        const accordionIndex = $('.elementor-accordion .elementor-accordion-item').index(currentPlayingItem.closest('.elementor-accordion-item'));
+        const nextPlayingItem = $(`.elementor-accordion .elementor-accordion-item:eq(${accordionIndex}) .playlist-item[data-index="${nextIndex}"]`);
+
+        if (nextPlayingItem.length > 0) {
+            currentPlayingItem.removeClass('playing');
+            currentPlayingItem = nextPlayingItem;
+            currentPlayingItem.addClass('playing');
+
+            const src = nextPlayingItem.data('src');
+            const title = nextPlayingItem.find('.podcast-title').text();
+            playPodcast(src, title);
+        } else {
+            resetPlayer();
+        }
+    });
+
+    // Set the first accordion as active
+    firstAccordion.trigger('click');
+	}
 });
